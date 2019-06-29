f7c28b1c0e26486f9350f46f74dbd41faafa98e1
aryyya <aryyya.xyz@gmail.com>
2019-06-29 00:33:47 -0700

    Fine tune style.

--- src/components/switch/switch.js
+++ src/components/switch/switch.js
@@ -5,17 +5,15 @@ import classNames from 'classnames'
 const Switch = ({
   isOn,
   onClick
-}) => {
-  return (
-    <div
-      className={classNames('switch', {
-        'switch--is-on': isOn
-      })}
-      onClick={onClick}
-    >
-      <div className="switch__inner" />
-    </div>
-  )
-}
+}) => (
+  <div
+    className={classNames('switch', {
+      'switch--is-on': isOn
+    })}
+    onClick={onClick}
+  >
+    <div className="switch__inner" />
+  </div>
+)
 
 export default Switch
